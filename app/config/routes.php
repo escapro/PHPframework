2530afcbb131454ddb3374f392614c4224a35684
ruslan4742 <36818965+ruslan4742@users.noreply.github.com>
2020-06-30 12:34:40 +0400

    pass

--- app/config/routes.php
+++ app/config/routes.php
@@ -10,3 +10,16 @@ $route->respond('GET', '/', function () {
 $route->respond('POST', '/post', function ($request) {
     var_dump(Input::post('q'));
 });
+
+$route->respond('GET', '/qwe', function ($request, $response) {
+	$obj = [
+		"names" => [
+			"Peter"=>35,
+			"Ben"=>37,
+			"Joe"=>43
+		]
+	];
+	
+	$send = $request->param('format', 'json');
+    $response->$send($obj);
+});
